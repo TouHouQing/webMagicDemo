fb1b4204dce17854d6215e951ae5feb6e2e516df
touhouqing <qinghaoyang@foxmail.com>
2025-07-22 11:34:07 +0800

    init

--- src/main/java/com/touhouqing/webmagicdemo/component/JobScheduler.java
+++ src/main/java/com/touhouqing/webmagicdemo/component/JobScheduler.java
@@ -1,4 +1,15 @@
 package com.touhouqing.webmagicdemo.component;
 
+import org.springframework.context.annotation.Bean;
+import us.codecraft.webmagic.scheduler.BloomFilterDuplicateRemover;
+import us.codecraft.webmagic.scheduler.QueueScheduler;
+import us.codecraft.webmagic.scheduler.Scheduler;
+
 public class JobScheduler {
+
+    @Bean("scheduler")
+    public Scheduler scheduler(){
+        BloomFilterDuplicateRemover bloomFilterDuplicateRemover = new BloomFilterDuplicateRemover(10000000);
+        return new QueueScheduler().setDuplicateRemover(bloomFilterDuplicateRemover);
+    }
 }
--- src/main/java/com/touhouqing/webmagicdemo/component/MyPipeline.java
+++ src/main/java/com/touhouqing/webmagicdemo/component/MyPipeline.java
@@ -1,4 +1,25 @@
 package com.touhouqing.webmagicdemo.component;
 
-public class MyPipeline {
+import com.touhouqing.webmagicdemo.entity.JobInfo;
+import com.touhouqing.webmagicdemo.mapper.JobInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+public class MyPipeline implements Pipeline {
+
+    @Autowired
+    private JobInfoMapper jobInfoMapper;
+
+
+    @Override
+    public void process(ResultItems resultItems, Task task) {
+        // 取出数据
+        JobInfo jobinfo = resultItems.get("jobinfo");
+
+        // 保存数据库
+        jobInfoMapper.insert(jobinfo);
+
+    }
 }
