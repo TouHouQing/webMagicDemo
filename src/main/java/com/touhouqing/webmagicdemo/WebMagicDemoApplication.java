aa2c9f226ddb37b82f4d0a4607a317ae57f7b968
touhouqing <qinghaoyang@foxmail.com>
2025-07-22 11:04:44 +0800

    init

---
+++ src/main/java/com/touhouqing/webmagicdemo/WebMagicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.touhouqing.webmagicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMagicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMagicDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/touhouqing/webmagicdemo/WebMagicDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.touhouqing.webmagicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMagicDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
