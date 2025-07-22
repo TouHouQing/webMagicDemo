b1369970964954dac4b7f4c2ddb274c4755c05d6
touhouqing <qinghaoyang@foxmail.com>
2025-07-22 11:06:10 +0800

    init

--- src/main/java/com/touhouqing/webmagicdemo/component/JobPageProcessor.java
+++ src/main/java/com/touhouqing/webmagicdemo/component/JobPageProcessor.java
@@ -1,4 +1,18 @@
 package com.touhouqing.webmagicdemo.component;
 
-public class JobPageProcessor {
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class JobPageProcessor implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    public Site getSite() {
+        Site size;
+        return null;
+    }
 }
